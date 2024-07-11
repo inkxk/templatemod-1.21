418b3a162bd23375d9c2866a170b9f8a91b26025
Potcharapol Nokyoo <potcharapol.nok@outlook.co.th>
2024-07-11 20:33:48 +0700

    [fix]: block texture bug

--- src/main/java/net/inkxk/templatemod/block/ModBlock.java
+++ src/main/java/net/inkxk/templatemod/block/ModBlock.java
@@ -8,7 +8,6 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlock {
@@ -30,12 +29,12 @@ public class ModBlock {
   }
 
   public static final Block RUBY_BLOCK = ModBlock.register(
-      new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)),
+      new Block(AbstractBlock.Settings.copy(Blocks.EMERALD_BLOCK)),
       "ruby_block",
       true);
 
   public static final Block RAW_RUBY_BLOCK = ModBlock.register(
-      new Block(AbstractBlock.Settings.copy(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)),
+      new Block(AbstractBlock.Settings.copy(Blocks.EMERALD_ORE)),
       "raw_ruby_block",
       true);
 }
